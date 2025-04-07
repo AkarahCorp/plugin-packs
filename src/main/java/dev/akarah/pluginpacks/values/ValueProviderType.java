e3cd3d9a5f8c6fc11c73841a8ee0f611ba6f8d14
akarahdev <endisticbutitsforgit@gmail.com>
2025-04-07 09:56:20 -0400

    feat: remove ValueProvider api, this should be left up to external dependencies

--- src/main/java/dev/akarah/pluginpacks/values/ValueProviderType.java
+++
@@ -1,8 +0,0 @@
-package dev.akarah.pluginpacks.values;
-
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.PrimitiveCodec;
-
-public record ValueProviderType<P extends ValueProvider<?>>(String name) {
-    public static Codec<ValueProviderType<?>> CODEC = PrimitiveCodec.STRING.xmap(ValueProviderType::new, ValueProviderType::name);
-}
